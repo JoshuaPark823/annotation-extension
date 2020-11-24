fe660c3f5fcd6f4ce31317262ca15eb0a2ac6762
Josh Park <joshuapark823@gmail.com>
2020-11-24 04:09:13 -0500

    attempting to interact with popup.html

--- foreground.js
+++ foreground.js
@@ -3,10 +3,11 @@ console.log("Foreground Injected");
 // Set a variable that points to both buttons, [0] := previous, [1] := next
 var prev_next = document.getElementsByClassName("mat-focus-indicator sentence-button mat-icon-button mat-button-base");
 
-var count = 0; // initialize the count as 0
+var count = 0; // initialize a temp count variable to use with each sentence
+var count_prev = 0;
+var total = 0; // initialize a variable to hold the user total
 var title_element; // Title element
 var title_string; // Enclosing string
-var init = 0; // init as 0
 var tags_applied = 0; // current number of tags applied (updates anytime the DOM subtree is modified)
 
 /**
@@ -23,11 +24,26 @@ function current_amount() {
 }
 
 /**
+ * @param count_input: new count for the popup to be updated to
+ * @return: none
+ */
+function display_count(count_input) {
+    var views = chrome.extension.getViews({
+        type: "popup"
+    });
+
+    for (var i = 0; i < views.length; i++) {
+        views[i].document.getElementById('display-count').innerHTML = count_input;
+    }
+}
+
+/**
  * DOM Subclass Event Listener (Count Logic)
  */
 document.getElementsByClassName("title")[0].addEventListener('DOMSubtreeModified', function() {
     
     var tags_before = tags_applied;
+    count_prev = count;
     tags_applied = current_amount(); // set the new number of tags
     console.log("c before " + count); // log the count, debug
 
@@ -42,14 +58,22 @@ document.getElementsByClassName("title")[0].addEventListener('DOMSubtreeModified
     }
 
     console.log("c after " + count); // log the count, debug
-    
 });
 
 /**
  * NEXT Event Listener
  */
 prev_next[1].addEventListener('click', function(e) {
-    console.log("the count after clicking next is: " + count);
+
+    tags_before = 0; // set t_b = 0 because we should theoretically be on a new sentence
+    
+    // This is necessary because clicking the next button modifies the DOM subtree.
+    // There's probs a way to do this w the chrome.storage API but eh, this is faster
+    total += count_prev;
+
+    display_count(total); // update the count on the popup to the new total
+
+    console.log("the total after clicking next is: " + total);
 });
 
 /**
